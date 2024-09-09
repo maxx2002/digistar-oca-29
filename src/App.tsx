339b10387948d4ae768f7b6047dd933aeca8c4ca
maxx2002 <max.wiranata@unitcode.org>
2024-09-09 22:48:11 +0700

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button variant="outline">Button</Button>
+    </div>
+  );
+}
+
+export default App;
