05df3e89a8ec03ab627bbd4d223da61750ee5cb9
maxx2002 <max.wiranata@unitcode.org>
2024-09-14 19:19:01 +0700

    added deploy.yml

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/digistar-oca-29">
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex flex-1">
